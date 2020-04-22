96a9e4d68375449a192672547c9eda1c1a8d2e65
Shane McDonough <shanemcdman@gmail.com>
2020-04-22 12:35:30 -0700

    Use spaces

--- Connections.js
+++ Connections.js
@@ -1,26 +1,26 @@
 class Connections{
-	constructor(){
-		this.frequency = 20;
-		this.opposite_offset = 2
-	}
+    constructor(){
+        this.frequency = 20;
+        this.opposite_offset = 2
+    }
 
-	draw_points(){
-		strokeWeight(5);
-		for(let i = 0; i < this.points.length; i++){
-			point(this.points[i])
-		}
-	}
+    draw_points(){
+        strokeWeight(5);
+        for(let i = 0; i < this.points.length; i++){
+            point(this.points[i])
+        }
+    }
 
-	connect_points(){
-		strokeWeight(1);
-		for(let i = 0; i < this.points.length; i++){
-			let opposite_index = (i + int(this.points.length / this.opposite_offset)) % this.points.length
-			line(this.points[i].x, this.points[i].y, this.points[opposite_index].x, this.points[opposite_index].y)
-		}
-	}
+    connect_points(){
+        strokeWeight(1);
+        for(let i = 0; i < this.points.length; i++){
+            let opposite_index = (i + int(this.points.length / this.opposite_offset)) % this.points.length
+            line(this.points[i].x, this.points[i].y, this.points[opposite_index].x, this.points[opposite_index].y)
+        }
+    }
 
-	draw(){
-		this.draw_points()
-		this.connect_points()
-	}
+    draw(){
+        this.draw_points()
+        this.connect_points()
+    }
 }
--- sketch.js
+++ sketch.js
@@ -2,68 +2,68 @@ let frequency_slider;
 let offset_slider;
 let star_point_slider;
 const shapes = {
-	NONE: null,
-	SQUARE: "square",
-	CIRCLE: "circle",
-	STAR: "star",
+    NONE: null,
+    SQUARE: "square",
+    CIRCLE: "circle",
+    STAR: "star",
 }
 
 function setup(){
-	createCanvas(600, 600).parent('cnvs');
-	stroke(255);
-	createP("Frequency").parent('controls');
-	frequency_slider = createSlider(1, 50, 20).parent('controls');
-	createP("Offset").parent('controls');
-	offset_slider = createSlider(1.1, 5, 2, 0.01).parent('controls');
-	connections = new SquareConnections();
-	star_div = createDiv().parent('controls');
-	createP("Points on star").parent(star_div);
-	star_point_slider = createSlider(1, 15, 5).parent(star_div);
-	star_div.style('display', 'none');
+    createCanvas(600, 600).parent('cnvs');
+    stroke(255);
+    createP("Frequency").parent('controls');
+    frequency_slider = createSlider(1, 50, 20).parent('controls');
+    createP("Offset").parent('controls');
+    offset_slider = createSlider(1.1, 5, 2, 0.01).parent('controls');
+    connections = new SquareConnections();
+    star_div = createDiv().parent('controls');
+    createP("Points on star").parent(star_div);
+    star_point_slider = createSlider(1, 15, 5).parent(star_div);
+    star_div.style('display', 'none');
 }
 
 function draw(){
-	background(0);
+    background(0);
     if(connections.type() == shapes.CIRCLE || connections.type() == shapes.STAR){
         translate(width / 2, height / 2);
-	}
-	connections.draw()
-	noLoop();
+    }
+    connections.draw()
+    noLoop();
 }
 
 function mousePressed(){
-	update_screen()
+    update_screen()
 }
 
 function mouseDragged(){
-	update_screen()
+    update_screen()
 }
 
 function update_screen(){
-	let new_freq = 51 - frequency_slider.value()
-	if(new_freq != connections.frequency_offset){
-		connections.frequency = new_freq;
-		connections.get_points();
-	}
-	if(offset_slider.value() != connections.opposite_offset){
-		connections.opposite_offset = offset_slider.value();
-	}
-	if(connections.type() == shapes.STAR){
-		star_div.style('display', 'block');
-		connections.number_of_star_points = star_point_slider.value();
-	}else{
-		star_div.style('display', 'none');
-	}
-	loop();
+    let new_freq = 51 - frequency_slider.value()
+    if(new_freq != connections.frequency_offset){
+        connections.frequency = new_freq;
+        connections.get_points();
+    }
+    if(offset_slider.value() != connections.opposite_offset){
+        connections.opposite_offset = offset_slider.value();
+    }
+    if(connections.type() == shapes.STAR){
+        star_div.style('display', 'block');
+        connections.number_of_star_points = star_point_slider.value();
+    }else{
+        star_div.style('display', 'none');
+    }
+    loop();
 }
 
 function swap_shapes(){
-	if(connections.type() == shapes.SQUARE){
-		connections = new CircleConnections();
-	}else if(connections.type() == shapes.CIRCLE){
-		connections = new StarConnections();
-	}else{
-		connections = new SquareConnections();
-	}
-	update_screen();
+    if(connections.type() == shapes.SQUARE){
+        connections = new CircleConnections();
+    }else if(connections.type() == shapes.CIRCLE){
+        connections = new StarConnections();
+    }else{
+        connections = new SquareConnections();
+    }
+    update_screen();
 }
