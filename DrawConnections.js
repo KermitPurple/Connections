2c9fc974a13042556f2a33c45049202304b7eca1
Shane McDonough <shanemcdman@gmail.com>
2020-05-19 20:31:10 -0700

    Add new point every click

--- DrawConnections.js
+++ DrawConnections.js
@@ -1,11 +1,18 @@
 class DrawConnections extends Connections{
     constructor(){
         super();
+        this.points = []
         this.get_points()
     }
 
     get_points(){
-        this.points = [];
+        if( mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height){
+            connections.add_point(createVector(mouseX, mouseY));
+        }
+    }
+
+    add_point(point){
+        this.points.push(point);
     }
 
     type(){
